79a7bc82b73fd0ca775e2b241388be9a6598bf0d
Denis Carriere <carriere.denis@gmail.com>
2016-12-31 01:21:09 -0500

    Clean tests

--- index.test.ts
+++ index.test.ts
@@ -2,43 +2,38 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { MBTiles, Metadata } from './'
 
-describe('Metadata', () => {
-  test('update', async () => {
-    const mbtiles = new MBTiles('update.mbtiles')
-    const metadata: Metadata = {
-      name: 'foo',
-      bounds: [-110, -20, 130, 30],
-    }
-    expect(await mbtiles.update(metadata)).toBeTruthy()
-    expect(await mbtiles.metadata()).toEqual(metadata)
-    fs.unlinkSync('update.mbtiles')
-  })
+describe('Metadata', async () => {
+  const mbtiles = new MBTiles('Metadata.mbtiles')
+  const metadata: Metadata = {
+    name: 'foo',
+    bounds: [-110, -20, 130, 30],
+  }
 
-  test('index', async () => {
-    const mbtiles = new MBTiles('index.mbtiles')
-    expect(await mbtiles.index()).toBeTruthy()
-    fs.unlinkSync('index.mbtiles')
-  })
+  test('update', async () => expect(await mbtiles.update(metadata)).toBeTruthy())
+
+  test('getMetadata', async () => expect(await mbtiles.metadata()).toEqual(metadata))
+
+  test('index', async () => expect(await mbtiles.index()).toBeTruthy())
+
+  test('init', async () => expect(await mbtiles.init()).toBeTruthy())
+
+  test('tables', async () => expect(await mbtiles.tables()).toBeTruthy())
 })
 
-describe('Save', async () => {
-  const mbtiles = new MBTiles('save.mbtiles')
+describe('CRUD', async () => {
+  const mbtiles = new MBTiles('CRUD.mbtiles')
   const tileData = fs.readFileSync(path.join(__dirname, 'fixtures', 'images', '0', '0', '0.png'))
-  const save = await mbtiles.save([0, 0, 0], tileData)
-  const deleted = await mbtiles.delete([0, 0, 0])
-  test('[0, 0, 0]', () => expect(save).toBeTruthy())
-  test('[0, 0, 0]', () => expect(deleted).toBeTruthy())
-  fs.unlinkSync('save.mbtiles')
+
+  test('noData', async () => expect(await mbtiles.tile([0, 0, 0])).toBeUndefined())
+
+  test('save', async () => expect(await mbtiles.save([0, 0, 0], tileData)).toBeTruthy())
+
+  test('read', async () => expect(await mbtiles.tile([0, 0, 0])).toEqual(tileData))
+
+  test('deleted', async () => expect(await mbtiles.delete([0, 0, 0])).toBeTruthy())
 })
 
-describe('Read', () => {
-  test('tile', async () => {
-    const mbtiles = new MBTiles(path.join(__dirname, 'fixtures', 'world.mbtiles'))
-    const data = await mbtiles.tile([0, 0, 0])
-    expect(data).toEqual(fs.readFileSync(path.join(__dirname, 'fixtures', 'images', '0', '0', '0.png')))
-  })
-  test('tile - error', async () => {
-    const mbtiles = new MBTiles(path.join(__dirname, 'fixtures', 'world.mbtiles'))
-    await mbtiles.tile([5, 0, 0]).catch(error => expect(error).toBeDefined())
-  })
+afterAll(() => {
+  fs.unlinkSync('CRUD.mbtiles')
+  fs.unlinkSync('Metadata.mbtiles')
 })
--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -1,20 +1,24 @@
 import * as path from 'path'
-import { getFiles, parseMetadata } from './utils'
+import * as fs from 'fs'
+import { getFiles, parseMetadata, createFolder } from './utils'
 
 describe('Utils', () => {
-  test('getFiles', () => {
-    expect(getFiles(path.join(__dirname, '..', 'fixtures'))).toBeDefined()
+  test('getFiles', () => expect(getFiles(path.join(__dirname, '..', 'fixtures'))).toBeDefined())
+  test('createFolder', async () => {
+    await createFolder('createFolderTest/test.mbtiles')
+    expect(fs.existsSync('createFolderTest')).toBeTruthy()
+    fs.rmdirSync('createFolderTest/')
   })
+})
 
-  test('parseMetadata', () => {
-    expect(parseMetadata([{name: 'center', value: '110,30,800'}])).toEqual({center: [110, 30, 800]})
-    expect(parseMetadata([{name: 'center', value: '110,30'}])).toEqual({center: [110, 30]})
-    expect(parseMetadata([{name: 'type', value: 'baselayer'}])).toEqual({type: 'baselayer'})
-    expect(parseMetadata([{name: 'name', value: 'foo'}])).toEqual({name: 'foo'})
-    expect(parseMetadata([{name: 'bounds', value: '-110,-20,130,30'}])).toEqual({bounds: [-110, -20, 130, 30]})
-    expect(parseMetadata([{name: 'format', value: 'png'}])).toEqual({format: 'png'})
-    expect(parseMetadata([{name: 'version', value: '1.1.0'}])).toEqual({version: '1.1.0'})
-    expect(parseMetadata([{name: 'minzoom', value: '10'}])).toEqual({minzoom: 10})
-    expect(parseMetadata([{name: 'maxzoom', value: '18'}])).toEqual({maxzoom: 18})
-  })
+describe('Metadata', () => {
+  test('center [x,y,z]', () => expect(parseMetadata([{name: 'center', value: '110,30,800'}])).toEqual({center: [110, 30, 800]}))
+  test('center [x,y]', () => expect(parseMetadata([{name: 'center', value: '110,30'}])).toEqual({center: [110, 30]}))
+  test('type', () => expect(parseMetadata([{name: 'type', value: 'baselayer'}])).toEqual({type: 'baselayer'}))
+  test('name', () => expect(parseMetadata([{name: 'name', value: 'foo'}])).toEqual({name: 'foo'}))
+  test('bounds', () => expect(parseMetadata([{name: 'bounds', value: '-110,-20,130,30'}])).toEqual({bounds: [-110, -20, 130, 30]}))
+  test('format', () => expect(parseMetadata([{name: 'format', value: 'png'}])).toEqual({format: 'png'}))
+  test('version', () => expect(parseMetadata([{name: 'version', value: '1.1.0'}])).toEqual({version: '1.1.0'}))
+  test('minzoom', () => expect(parseMetadata([{name: 'minzoom', value: '10'}])).toEqual({minzoom: 10}))
+  test('maxzoom', () => expect(parseMetadata([{name: 'maxzoom', value: '18'}])).toEqual({maxzoom: 18}))
 })
